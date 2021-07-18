047b9d06d7dd519e091ad6c7b27a1d43dea50f12
Leonardo Souza <leonardo.souza@lambda3.com.br>
2021-07-18 14:20:28 -0300

    Criando classe Entity

---
+++ src/LeoStore.Catalogo.Domain/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeoStore.Catalogo.Domain
+{
+    public class Produto
+    {
+    }
+
+    public class Categoria
+    {
+    }
+}
---
+++ src/LeoStore.Core/DomainObjects/Entity.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace LeoStore.Core.DomainObjects
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var compareTo = obj as Entity; // casting
+
+            // comparando tipo
+            if (ReferenceEquals(this, compareTo)) return true;
+            if (ReferenceEquals(null, compareTo)) return true;
+
+            // comparando Id
+            return Id.Equals(compareTo.Id);
+        }
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            // multiplica por um valor aleatório (907) e soma o hashCode do Id
+            return (GetType().GetHashCode() * 907) + Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} [Id ={Id}]";
+        }
+    }
+}
